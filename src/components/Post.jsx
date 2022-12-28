8596cbbebd64dec538d989d71248520f3f1d8337
*Gustavo <guga77sugu@hotmail.com>
2022-12-28 14:26:59 -0300

    styles finished.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/joyceorlandini.png" alt="" />
+                    <Avatar hasBorder src="https://github.com/joyceorlandini.png" alt="" />
                     <div className={styles.authorInfo}>
                         <strong>Joyce Ammirati</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 import styles from "./Sidebar.module.css"; 
 
 
@@ -11,7 +12,7 @@ export function Sidebar() {
                 alt=""       
             />       
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Gustavo-Orlandini.png" />         
+                <Avatar src="https://github.com/Gustavo-Orlandini.png" />         
                 
                 <strong>Gustavo Orlandini</strong>         
                 <span>Web Developer</span>       
